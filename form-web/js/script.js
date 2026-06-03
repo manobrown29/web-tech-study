16f855f9a5922ab3230cc65fd4d1143ab1eabfaf
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-06-03 11:15:58 -0300

    feat: finalizando layout css e adc JS no projeto

---
+++ form-web/js/script.js
@@ -0,0 +1,8 @@
+const nome = document.querySelector("#nome");
+const button = document.querySelector("button");
+const lista = document.querySelector(".lista");
+
+button.addEventListener("click", (event) => {
+    event.preventDefault();
+    
+}) 
