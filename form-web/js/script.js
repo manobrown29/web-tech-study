68463b14a853acc898bd78342cee21880a620711
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-06-03 11:28:48 -0300

    fat: finalizando a parte do JS no campo nome

--- form-web/js/script.js
+++ form-web/js/script.js
@@ -1,8 +1,14 @@
 const nome = document.querySelector("#nome");
 const button = document.querySelector("button");
-const lista = document.querySelector(".lista");
+const lista = document.querySelector(".list");
 
 button.addEventListener("click", (event) => {
     event.preventDefault();
+    const inputValue = nome.value;
+    const templateHTML = `<li>${inputValue}</li>`;
     
-}) 
+    lista.innerHTML += templateHTML;
+
+    nome.value = "";
+});
+
