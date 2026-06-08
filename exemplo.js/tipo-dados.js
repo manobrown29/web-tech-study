ed041d2874024b424a676a192a93476036628404
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-06-08 10:45:31 -0300

    tipos de variaveis

---
+++ exemplo.js/tipo-dados.js
@@ -0,0 +1,18 @@
+//String
+let nome = "João Pedro"
+
+//Inteiro
+let idade = 18;
+
+//Booleano
+let casado = false;
+
+//Números flutuantes com casas decimais
+let valorProduto = 5.55;
+
+//Resultados
+console.log("Meu nome é: " + nome);
+console.log("Minha idade é: " + idade);
+console.log("Sou casado: " + casado);
+console.log("Quanto eu valho: R$" + valorProduto);
+console.log(typeof nome);
