5ee271fbdfc8653da57b742b3fa07a8a0dd49a15
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-06-08 11:20:28 -0300

    exercicio 1

---
+++ exemplo.js/exer1.js
@@ -0,0 +1,16 @@
+/*
+    - Armazene a palavra 'Tom' em uma let "tom"; 
+    - Armazene a palavra 'Hanks' em uma let "firstTom";
+    - Armazene a palavra 'Cruise' em uma let "secondTom";
+    - Declare uma let "myFavoriteTom" que recebe nome, 
+        espaço, e sobrenome do Tom que você mais gosta;
+    - Exiba a "myFavoriteTom" no console do browser.
+*/
+
+let tom = "Tom";
+let firtTom = "Hanks";
+let secondTom = "Cruise";
+
+//let myFavoriteTom = tom + " " + firtTom + " " + secondTom;
+let myFavoriteTom = `${tom} ${firtTom} ${secondTom}`;
+console.log(myFavoriteTom);
