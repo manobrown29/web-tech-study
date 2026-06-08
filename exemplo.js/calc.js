6491f9518ab324a52563e2e7d4ea7dc6016450f5
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-06-08 11:01:48 -0300

    calc com js

---
+++ exemplo.js/calc.js
@@ -0,0 +1,12 @@
+let num1 = 10;
+let num2 = 10;
+
+let soma = num1 + num2;
+let Subtracao = num1 - num2;
+let divisao = num1 / num2;
+let multiplicacao = num1 * num2;
+
+console.log("Soma", soma);
+console.log("Subtração", Subtracao);
+console.log("Divisão", divisao);
+console.log("Multiplicação", multiplicacao);
