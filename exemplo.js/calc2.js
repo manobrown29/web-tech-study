9507bc6cf1945ebb753ecf7d284b627dd1364f4c
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-06-10 09:04:49 -0300

    feat: fazendo somas no console.log usandoo a tag prompt

---
+++ exemplo.js/calc2.js
@@ -0,0 +1,14 @@
+let num1 = Number(prompt("Digite um número:"));
+let num2 = Number(prompt("Digite outro número número:"));
+
+let soma = num1 + num2
+let sub = num1 - num2
+let div = num1 / num2
+let mul = num1 * num2
+
+console.log(soma);
+console.log(sub);
+console.log(div);
+console.log(mul);
+
+console.log(typeof soma);
