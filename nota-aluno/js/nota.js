142580e50de455a260a3fa32324e0aafe66a53fe
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-06-10 11:36:39 -0300

    feat: termino do nota aluno

--- nota-aluno/js/nota.js
+++ nota-aluno/js/nota.js
@@ -1,18 +1,21 @@
 const inputNota = document.getElementById("nota")
 const resultado = document.getElementById("resultado")
+const message = document.getElementById("message")
 
 function resultadoNotaClick(event){
     event.preventDefault();
     const inputNotaValue = inputNota.value
 
     if(inputNotaValue == ""){
-        alert("Digite a nota!!")
+        message.style.display = "block";
         return false;
     }
     
-    if(inputNota.value >= 6){
-        alert("O aluno foi aprovado")
+    if(inputNotaValue >= 6){
+        resultado.innerHTML = "Aprovado"
     }else{
-        alert("O aluno foi reprovado")
+        resultado.innerHTML = "Reprovado"
     }
+
+    inputNota.value = "";
 }       
