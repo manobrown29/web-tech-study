9e389be567830cfe6f20aceb73b533e4cd00d12c
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-06-15 11:35:23 -0300

    feat: POPUP finalizado

--- pop-up/js/script.js
+++ pop-up/js/script.js
@@ -1,5 +1,7 @@
 const button = document.querySelector("button")
 const popup = document.querySelector(".popup-wrapper")
+const popupLink = document.querySelector(".popup-link")
+const CorFundo = document.querySelector("body")
 
 button.addEventListener("click", function(event) {
     popup.classList.add("d-block")
@@ -9,7 +11,13 @@ popup.addEventListener("click", function(event){
     const classOfClickElement = event.target.classList[0] //Indentifica a classe aonde clica
     const classNameListArray = ["popup-close", "popup-wrapper"]
 
-    if(classNameListArray.includes(classOfClickElement)){
+    const isClassList = classOfClickElement === "popup-close" || classOfClickElement === "popup-wrapper"
+
+    if(isClassList){
         popup.classList.remove("d-block")
     }
+})
+
+popupLink.addEventListener("click", function(event){
+    CorFundo.classList.add("dark")
 })
