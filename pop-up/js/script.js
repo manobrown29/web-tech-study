15e696f234ba1aada5d5bbf3096de80d279760d7
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-06-15 10:41:17 -0300

    feat: POPUP aba fechar e abrir pronta

--- pop-up/js/script.js
+++ pop-up/js/script.js
@@ -1,3 +1,15 @@
-let times = ["Palmeiras", "Brasil", "Portugal"];
+const button = document.querySelector("button")
+const popup = document.querySelector(".popup-wrapper")
 
-console.log(times.includes("Palmeiras"))
+button.addEventListener("click", function(event) {
+    popup.classList.add("d-block")
+});
+
+popup.addEventListener("click", function(event){
+    const classOfClickElement = event.target.classList[0] //Indentifica a classe aonde clica
+    const classNameListArray = ["popup-close", "popup-wrapper"]
+
+    if(classNameListArray.includes(classOfClickElement)){
+        popup.classList.remove("d-block")
+    }
+})
