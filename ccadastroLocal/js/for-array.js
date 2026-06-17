9c55ac84d0c520e141287fc40d9ca92871a5cb76
GABRIEL TEIXEIRA ALVES DAMACENO <gabriel.damaceno4@senaisp.edu.br>
2026-06-17 11:14:46 -0300

    Feat: adicionando codigo e exemplos de FOR

---
+++ ccadastroLocal/js/for-array.js
@@ -0,0 +1,5 @@
+const times = ["Bangu", "Santos", "Palmeiras"];
+
+for(let i = 0; i <times.length; i++){
+    console.log("Times: " + i + "-" + times[i])
+}
---
+++ ccadastroLocal/js/listagem.js
@@ -0,0 +1,10 @@
+const listaValores = document.querySelector("#listaValores")
+
+//Carregar valores ao iniciar a página
+carregarValoresSalvos();
+
+
+//Função para carregar valores na lista
+function carregarValoresSalvos(){
+    console.log("carregarValoresSalvos")
+}
