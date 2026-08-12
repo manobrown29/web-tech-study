ea6bd3756debc53a074b772c7c75a03e1a507b6b
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-08-12 10:51:06 -0300

    componente Card.jsx e card.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
 import Header from "./Components/Header"
+import Card from "./Components/Card"
 
 function App() {
 
   return (
     <>
       <Header />
-      
-      <section>
-          <article>
-            <h1>Lorem Ipsun</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, dolore, similique maiores cumque neque quo alias cum voluptatem dolorum perferendis obcaecati veritatis ad vero doloremque at sed accusantium totam inventore!</p>
-          </article>
 
-          <article>
-            <h1>Lorem Ipsun</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, dolore, similique maiores cumque neque quo alias cum voluptatem dolorum perferendis obcaecati veritatis ad vero doloremque at sed accusantium totam inventore!</p>
-          </article>
-          <hr />
-
+      <Card />
           <footer>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe non assumenda
           </footer>
-      </section>
     </>
   )
 }
---
+++ src/Components/Card.jsx
@@ -0,0 +1,17 @@
+import imgCard from "../assets/img-card.jpg"
+import "./card.css"
+
+
+const Card = () => {
+    return (
+        <section>
+            <article className="card">
+                <img src={imgCard} className="img-card"/>
+                <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus, dolore, similique maiores cumque neque quo alias cum voluptatem dolorum perferendis obcaecati veritatis ad vero doloremque at sed accusantium totam inventore!</p>
+            </article> 
+          <hr />
+        </section>
+    )
+}
+
+export default Card;
