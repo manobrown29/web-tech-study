22f1da1260d48b202c7a9dc95af71352cb049420
manobrown29 <143205236+manobrown29@users.noreply.github.com>
2026-09-02 09:30:01 -0300

    feat: começo do projeto react

---
+++ projeto-react/src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function index() {
+  return (
+    <header className='header'>
+        <section className='title-container'>
+            <h1 className='title'>Titulo principal</h1>
+            <h2>Lorem ipsum</h2>
+        </section>
+    </header>
+  )
+}
